4dcd0b6e65deabc8f210f4be9b3db0f338c8d5d3
Burak Eregar <burakeregar@gmail.com>
2022-05-17 00:08:09 +0100

    Add firebase auth dependency

--- app/src/main/java/com/emrecantezguren/todolist/loginscreen/LoginScreen.kt
+++ app/src/main/java/com/emrecantezguren/todolist/loginscreen/LoginScreen.kt
@@ -5,20 +5,43 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.emrecantezguren.todolist.databinding.ActivityLoginScreenBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginScreenBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupOnClickListeners()
+        auth = Firebase.auth
     }
 
     private fun setupOnClickListeners(){
+        binding.loginButton.setOnClickListener {
+            auth.createUserWithEmailAndPassword(binding.username.text.toString(), binding.password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+//                        Log.d(TAG, "createUserWithEmail:success")
+//                        val user = auth.currentUser
+//                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+//                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+//                        Toast.makeText(baseContext, "Authentication failed.",
+//                            Toast.LENGTH_SHORT).show()
+//                        updateUI(null)
+                    }
+                }
 
+        }
     }
 
     //anti-return to animation
